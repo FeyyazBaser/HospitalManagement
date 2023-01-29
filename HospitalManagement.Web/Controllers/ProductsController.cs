7bab87e31a6defe5d1373e3db24e6af6a91b3c5b
DESKTOP-0RKVR8U\BAŞER <furkan.biyik@moralabs.com>
2023-01-29 16:11:29 +0300

    Web controllerlar eklendi

---
+++ HospitalManagement.Web/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using HospitalManagement.Core.DTOs;
+using HospitalManagement.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using static System.Net.WebRequestMethods;
+
+namespace HospitalManagement.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ HospitalManagement.Web/Controllers/WareHousesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagement.Web.Controllers
+{
+    public class WareHousesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
